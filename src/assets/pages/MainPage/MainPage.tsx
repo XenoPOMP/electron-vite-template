abe9be3bb63fa1e982250302722f7674ff233972
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-04-30 14:58:48 +0300

    add localization

--- src/assets/pages/MainPage/MainPage.tsx
+++ src/assets/pages/MainPage/MainPage.tsx
@@ -1,22 +1,27 @@
 import cn from 'classnames';
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import Layout from '@components/Layout/Layout';
 import Page from '@components/Page/Page';
 
+import useAppSettings from '@hooks/useAppSettings';
+import useLocalization from '@hooks/useLocalization';
+
 import styles from './MainPage.module.scss';
 import type { MainPageProps } from './MainPage.props';
 
 const MainPage: FC<MainPageProps> = ({}) => {
+	const loc = useLocalization();
+	const { language } = useAppSettings();
+
 	return (
-		<Page
-			meta={{
-				pageTitle: 'Main',
-				pageDescription: '',
-				keywords: '',
-			}}
-		></Page>
+		<Page meta={loc.meta.mainPage}>
+			<button onClick={() => language.set('ru')}>Switch to russian</button>
+			<button onClick={() => language.set('en')}>Switch to english</button>
+
+			<h1>{loc.pages.main.label}</h1>
+		</Page>
 	);
 };
 
--- src/assets/redux/reducers/appSettingsSlice.ts
+++ src/assets/redux/reducers/appSettingsSlice.ts
@@ -2,22 +2,28 @@ import { createSlice } from '@reduxjs/toolkit';
 
 import { ReduxAction } from '@redux/types/redux-types';
 
+export type Language = 'en' | 'ru';
+
 export type AppSettings = {
 	appVersion: string;
+	language: Language;
 };
 
 const initialState: AppSettings = {
 	appVersion: '1.0.0',
+	language: 'en',
 };
 
 const appSettingsSlice = createSlice({
 	name: 'appSettings',
 	initialState,
 	reducers: {
-		simpleAction(state, action: ReduxAction<any>) {},
+		changeLang(state, action: ReduxAction<any>) {
+			state.language = action.payload;
+		},
 	},
 });
 
 export default appSettingsSlice.reducer;
-export const { simpleAction } = appSettingsSlice.actions;
+export const { changeLang } = appSettingsSlice.actions;
 export const initialAppSettings = appSettingsSlice.getInitialState();
