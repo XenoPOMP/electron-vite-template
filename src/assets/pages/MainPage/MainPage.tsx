910cad1df15771c8011dcd6ad749a7bba4a6e321
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-04-30 22:52:15 +0300

    delete unusing imports

--- src/assets/pages/MainPage/MainPage.tsx
+++ src/assets/pages/MainPage/MainPage.tsx
@@ -1,11 +1,8 @@
 import cn from 'classnames';
 import { FC, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 
-import Layout from '@components/Layout/Layout';
 import Page from '@components/Page/Page';
 
-import useAppSettings from '@hooks/useAppSettings';
 import useLocalization from '@hooks/useLocalization';
 
 import styles from './MainPage.module.scss';
@@ -13,16 +10,8 @@ import type { MainPageProps } from './MainPage.props';
 
 const MainPage: FC<MainPageProps> = ({}) => {
 	const loc = useLocalization();
-	const { language } = useAppSettings();
 
-	return (
-		<Page meta={loc.meta.mainPage}>
-			<button onClick={() => language.set('ru')}>Switch to russian</button>
-			<button onClick={() => language.set('en')}>Switch to english</button>
-
-			<h1>{loc.pages.main.label}</h1>
-		</Page>
-	);
+	return <Page meta={loc.meta.mainPage}></Page>;
 };
 
 export default MainPage;
